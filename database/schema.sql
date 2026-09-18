7a8b8b796bb09ab7a7360a847215fc9e16b44b98
fritzclssmbb <ffs.newvent@gmail.com>
2026-09-18 11:56:17 +0800

    db: enable citext required by user and lead email columns

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;\nCREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE organizations (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
